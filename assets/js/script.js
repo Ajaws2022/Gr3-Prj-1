b626e38a593845b800328066c2a1e1dbd2420528
kraemz303 <kraemz303@gmail.com>
2022-10-24 18:58:46 -0700

    added 2nd JS file and added pseudocode in JS file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//establish variables
+
+// create an array of food groups/ingredients for search bar
+
+//when user searches item(s) fetch api to display on the screen 
+
+//when video and/or article is added to page, user can save video
---
+++ assets/js/script2.js
@@ -0,0 +1,4 @@
+//saved videos and/or articles will display here
+
+
+//user will be able to remove videos and/or article whenever with click button
