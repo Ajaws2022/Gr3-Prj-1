11165eb9da8817d31b67e2de79a686d93597a8a5
kraemz303 <kraemz303@gmail.com>
2022-10-30 11:21:36 -0700

    restore original HTML changes and removal of a comment in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 //establish variables
 
-// create an array of food groups/ingredients for search bar
-
 //when user searches item(s) fetch api to display on the screen 
 
 //when video and/or article is added to page, user can save video
