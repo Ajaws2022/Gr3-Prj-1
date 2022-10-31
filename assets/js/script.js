ce248d30ceb9c4cb02e0c46a97f787785cecc747
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-30 21:15:43 -0700

    Updated Html and added Javascript to search/retrieve recipes

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '53df368d57msh1f4523eee823f73p114681jsn59f031fdad1b',
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+	}
+};
+
+var searchBTN = document.querySelector('#search')
+
+function getID(data){
+    for(i = 0; i < 10; i++){
+
+        const id  = data.results[i].id
+        const name = data.results[i].name
+        const desc = data.results[i].description
+        console.log(id)
+
+        let resultBox = document.querySelector('.resultBox');
+        
+        let card = document.createElement('div');
+        card.className = 'recCard';
+        
+        card.classList = "recCard card tile is-vertical column is-one-fifth m-3";
+        let recName = document.createElement('h3')
+        recName.classList = "is-size-3"
+        recName.innerHTML = name;
+        card.appendChild(recName)
+        let recDesc = document.createElement('p')
+        recDesc.innerHTML = desc;
+        card.appendChild(recDesc)
+        let saveBtn = document.createElement('button')
+        saveBtn.textContent = "Save Recipe"
+        card.appendChild(saveBtn)
+        resultBox.appendChild(card)
+    }
+}
+
+function getURL(){
+    var key = "53df368d57msh1f4523eee823f73p114681jsn59f031fdad1b"
+    var inputValue = document.getElementById('recSearch').value
+
+    var searchURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes,easy'+'&q='+inputValue +'&rapidapi-key='+key
+
+    console.log(inputValue)
+
+    fetch(searchURL, options)
+	.then(response => response.json())
+    // .then(data => console.log(data))
+	// .then(data => console.log(data.results[0].id))
+    .then((data) => getID(data))
+        
+    
+	// .catch(err => console.error(err));
+
+}
+
+function removeRecipes(){
+    let remArea= document.querySelector('.resultBox')
+    let remCard = document.getElementsByClassName('recCard')
+    console.log(remCard)
+    remArea.childNodes.forEach(remCard => remCard.remove())
+}
+
+let timesClicked = 0;
+
+function clicked(){
+ timesClicked++;
+ if (timesClicked>1){
+    removeRecipes();
+    getURL();
+ }else {
+    getURL();
+ }
+} 
+
+searchBTN.addEventListener('click', clicked)
